505b828730ef98b50842e87d1167b4e0ffdd8912
User@DESKTOP-HFA7F9M.(none) <Orlando.Malo@fti.edu.al>
2022-01-07 17:38:46 +0100

    added authconfig class and updated it

---
+++ SecureClient/AuthConfig.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureClient
+{
+    internal class AuthConfig
+    {
+        public string Instance { get; set; }
+        public string TenantId { get; set; }
+        public string ClientId { get; set; }
+        public string Authority
+        {
+            get
+            {
+                return String.Format(CultureInfo.InvariantCulture, Instance, TenantId);
+
+            }
+        }
+
+        public string ClientSecret { get; set; }
+        public string BaseAddress { get; set; }
+        public string ResourceId { get; set; }
+
+
+        public static AuthConfig ReadJsonFromFile(string path)
+        {
+            IConfiguration Configuration;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(path);
+
+            Configuration=builder.Build();
+
+            return Configuration.Get<AuthConfig>(); 
+        }
+    }
+}
--- SecureClient/Program.cs
+++ SecureClient/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SecureClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            AuthConfig config = AuthConfig.ReadJsonFromFile("appsettings.json.txt");
+            Console.WriteLine($"Authority: {config.Authority}");
+        }
+    }
+}
 
